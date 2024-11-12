6aac824476bee985a23a0b20395702eb90a5a13b
JMCN <joaomouratocn@gmail.com>
2024-11-12 00:16:21 -0300

    Adicionado banco de dados

---
+++ app/src/main/java/br/com/devjmcn/desafioguarani/model/DataBaseHelper.java
@@ -0,0 +1,64 @@
+package br.com.devjmcn.desafioguarani.model;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class DataBaseHelper extends SQLiteOpenHelper {
+    private static final String DB_NAME = "database/bancomovel.db";
+    private static final int DB_VERSION = 1;
+    private static String DB_PATH = "";
+    private final Context context;
+    private SQLiteDatabase  DB;
+
+    public DataBaseHelper(Context context){
+        super(context, DB_NAME, null, DB_VERSION);
+        this.context = context;
+        DB_PATH = context.getApplicationInfo().dataDir + "/database/";
+    }
+
+    public SQLiteDatabase openDatabase() {
+        if (DB == null || !DB.isOpen()) {
+            DB = SQLiteDatabase.openDatabase(DB_PATH + DB_NAME, null, SQLiteDatabase.OPEN_READWRITE);
+        }
+        return DB;
+    }
+
+    public void createDB(){
+        boolean dbExists = checkDataBase();
+        if (!dbExists) {
+            this.getReadableDatabase();
+            copyDB();
+        }
+    }
+
+    private void copyDB() {
+        try (InputStream input = context.getAssets().open(DB_NAME);
+             OutputStream output = new FileOutputStream(DB_PATH + DB_NAME)) {
+
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = input.read(buffer)) > 0) {
+                output.write(buffer, 0, length);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao importar banco de dados!!", e);
+        }
+    }
+
+    private boolean checkDataBase() {
+        File dbFile = new File(DB_PATH + DB_NAME);
+        return dbFile.exists();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {}
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {}
+}
